1330ad1596560c53ec9354d1f10c849652c8cb55
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 19:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber){
+  if (0 === cardNumber){
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     let msg = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
